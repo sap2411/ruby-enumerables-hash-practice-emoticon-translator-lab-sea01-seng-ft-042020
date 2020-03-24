6ab64bc370e4c9dbd0d2b114963abe96aa2cf2dc
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 05:31:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(lib)
   result
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
    library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
     library["get_emoticon"][english_emoticon]
